bd15e7a79d0575ecc141de01c4c64a323b039e73
ilya_tolkunov <ilya4249@gmail.com>
2021-09-02 23:19:09 +0300

    dm

---
+++ project0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Russki vodka balalayka");
+            Console.WriteLine("putin");
+        }
+    }
+}
